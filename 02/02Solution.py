8f0d799ee64eb13c57d193d95ee1bac9447e94ca
Daniel <djricky40@hotmail.com>
2020-12-02 13:50:35 +0000

    Update 02Solution.py
    changes to the if statment to catch more instances of the password so that some passwords don't sneak through.

--- 02/02Solution.py
+++ 02/02Solution.py
@@ -16,11 +16,11 @@ for y in _list:
     passwordlist.append(passwords(int(y[0]),int(y[1]),y[2],y[3]))
 
 count = 0
-badpass = 0
+
 for t in passwordlist:
-    if t.password[t._min - 1] == t._query and t.password[t._max - 1] != t._query:
+    if (t.password[t._min - 1] == t._query and t.password[t._max - 1] != t._query) or (t.password[t._min -1] != t._query and t.password[t._max - 1] == t._query):
         print(t.password[t._min - 1] + " position " + str(t._min) + " " + t.password[t._max - 1] + " position " + str(t._max) + " " + t.password)
-        count = count + 1
+        count += 1
         #if x == t._query:
         #    count = count +1
     #if count > t._max or count < t._min:
